f99bb5d20e2a51a543c96cb040affc5a3d03fd0f
Douglas <khembo.douglas.juma@gmail.com>
2024-07-30 00:11:21 +0300

    added project views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import *
+from .import views
 urlpatterns = [
-    path('', Home)
+    path('', Home),
+    path('project/create/',views.ProjectCreateView, name='createProject' ),
+    path('projects/', views.ProjectListView, name='listProjects'),
+    path('project/update/<int:pk>', views.ProjectDetailView, name='updateProject'),
+    path('project/delete/<int:pk>', views.ProjectDeleteView, name='deleteProject'),
+    path('project/detail/<int:pk>', views.ProjectDetailView, name='detailProject'),
 ]
